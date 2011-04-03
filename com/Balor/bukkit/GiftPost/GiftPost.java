7d10cff5e2cf09bffa7330989e0a4953578e545c
Antoine Aflalo <antoineaf@gmail.com>
2011-04-03 15:52:18 +0200

    creating new class GiftPost and VirtualChest

--- com/Balor/bukkit/GiftPost/GiftPost.java
+++ com/Balor/bukkit/GiftPost/GiftPost.java
@@ -2,13 +2,103 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.Balor.bukkit.GiftPost;
+
+import com.aranai.virtualchest.VirtualChest;
+import java.util.logging.Logger;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.Plugin;
+//Permissions imports
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import java.io.Serializable;
+import java.util.HashMap;
 
 /**
  *
  * @author Antoine
  */
-public class GiftPost {
+public class GiftPost extends JavaPlugin
+{
 
+    private HashMap<Player, VirtualChest> chests;
+    public static final Logger log = Logger.getLogger("Minecraft");
+    /**
+     * Permission plugin
+     */
+    private static PermissionHandler Permissions = null;
+
+    /**
+     * Checks that Permissions is installed.
+     */
+    private void setupPermissions()
+    {
+
+        Plugin perm_plugin = this.getServer().getPluginManager().getPlugin("Permissions");
+        PluginDescriptionFile pdfFile = this.getDescription();
+
+        if (Permissions == null)
+            if (perm_plugin != null)
+            {
+                //Permissions found, enable it now
+                this.getServer().getPluginManager().enablePlugin(perm_plugin);
+                Permissions = ((Permissions) perm_plugin).getHandler();
+                log.info("[" + pdfFile.getName() + "]" + " (version " + pdfFile.getVersion() + ") Enabled with Permissions.");
+            } else
+            {
+                log.info("[" + pdfFile.getName() + "]" + " (version " + pdfFile.getVersion() + ") Enables without Permissions.");
+                log.info("[" + pdfFile.getName() + "]" + " Instead of Permissions, check if the user is OP.");
+            }
+    }
+
+    /**
+     * Check the permissions
+     * @param player
+     * @param perm
+     * @return boolean
+     */
+    private boolean hasPerm(Player player, String perm)
+    {
+        if (Permissions == null)
+            return true;
+        else
+            return Permissions.has(player, perm);
+    }
+
+    @Override
+    public void onEnable()
+    {
+        setupPermissions();
+        chests = new HashMap<Player, VirtualChest>();
+    }
+
+    @Override
+    public void onDisable()
+    {
+        PluginDescriptionFile pdfFile = this.getDescription();
+        log.info("[" + pdfFile.getName() + "]" + " Plugin Disabled. (version" + pdfFile.getVersion() + ")");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args)
+    {
+        if (!(sender instanceof Player))
+        {
+            sender.sendMessage(ChatColor.RED + "You have to be a player!");
+            return true;
+        } else
+        {
+            Player player = (Player) sender;
+
+            String cmd = command.getName();
+
+
+        }
+        return false;
+    }
 }
--- com/aranai/virtualchest/VirtualChest.java
+++ com/aranai/virtualchest/VirtualChest.java
@@ -2,74 +2,52 @@
  * VirtualChest is a proof of concept for virtual chests in Bukkit
  * VC creates a single large global chest, accessible by all players via the /chest command
  * 
- * I have not verified the following:
- * 1. Proper functionality if multiple users access the chest simultaneously
- * 2. Proper triggering of available inventory hooks
  */
 package com.aranai.virtualchest;
-
-import java.util.logging.Logger;
 
 import net.minecraft.server.EntityPlayer;
 import net.minecraft.server.InventoryLargeChest;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 
 /**
  * VirtualChest for Bukkit
  *
- * @author Timberjaw
+ * @authors Timberjaw and Balor
  * 
  */
-public class VirtualChest extends JavaPlugin {
+public class VirtualChest
+{
 
-	private Logger log;
-	private TileEntityVirtualChest chest;
-	private TileEntityVirtualChest chest2;
-	private InventoryLargeChest lc;
+    private TileEntityVirtualChest chest;
+    private TileEntityVirtualChest chest2;
+    private InventoryLargeChest lc;
+    private Player owner;
 
-	@Override
-	public void onDisable() {}
+    public void VirtualChest(Player player)
+    {
+        // Large chests are made up of two individual small chests
+        // TileEntityVirtualChest extends the TileEntityChest class to remove some bothersome world checks
+        // This would NOT work with regular TileEntityChest instances
+        chest = new TileEntityVirtualChest();
+        chest2 = new TileEntityVirtualChest();
 
-	@Override
-	public void onEnable() {
-		log = Logger.getLogger("Minecraft");
-		
-		// Large chests are made up of two individual small chests
-		// TileEntityVirtualChest extends the TileEntityChest class to remove some bothersome world checks
-		// This would NOT work with regular TileEntityChest instances
-		chest = new TileEntityVirtualChest();
-		chest2 = new TileEntityVirtualChest();
-		
-		// Set up the global chest
-		// Note: this is NOT persisted across server restarts
-		lc = new InventoryLargeChest("Large chest", chest, chest2);
-		
-		// Enable message
-        PluginDescriptionFile pdfFile = this.getDescription();
-		log.info("[VirtualChest] version [" + pdfFile.getVersion() + "] loaded" );
-	}
-	
-	@Override
-    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-		// Show chest to the player. -_-
-		
-		// Verify that the sender is a player
-		if(sender instanceof Player)
-		{
-			// Get the EntityPlayer handle from the sender
-			EntityPlayer eh = ((CraftPlayer)sender).getHandle();
-			
-			// Chest time!
-			eh.a(lc);
-		}
-		
-		return true;
-	}
+        // Set up the global chest
+        // Note: this is NOT persisted across server restarts
+        owner = player;
+        lc = new InventoryLargeChest(owner.getName() + "'s PostChest", chest, chest2);
 
+    }
+
+    public boolean isBelongTo(Player player)
+    {
+        return owner.equals(player);
+    }
+
+    public void openChest()
+    {
+        EntityPlayer eh = ((CraftPlayer) owner).getHandle();
+        eh.a(lc);
+    }
 }
