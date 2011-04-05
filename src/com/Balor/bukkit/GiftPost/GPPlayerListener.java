f6b06e4c73ad5e084f6d32768117a893c8a19a28
Antoine Aflalo <antoineaf@gmail.com>
2011-04-05 16:13:55 +0200

    added a listener when a player join

---
+++ src/com/Balor/bukkit/GiftPost/GPPlayerListener.java
@@ -0,0 +1,42 @@
+/************************************************************************
+ * This file is part of GiftPost.									
+ *																		
+ * GiftPost is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by	
+ * the Free Software Foundation, either version 3 of the License, or		
+ * (at your option) any later version.									
+ *																		
+ * GiftPost is distributed in the hope that it will be useful,	
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of		
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
+ * GNU General Public License for more details.							
+ *																		
+ * You should have received a copy of the GNU General Public License
+ * along with GiftPost.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+package com.Balor.bukkit.GiftPost;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerListener;
+
+/**
+ * @author Balor (aka Antoine Aflalo)
+ * 
+ */
+public class GPPlayerListener extends PlayerListener {
+	private GiftPostWorker worker;
+
+	public GPPlayerListener(GiftPostWorker gpw) {
+		worker = gpw;
+	}
+
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		if (!worker.getChest(event.getPlayer().getName()).isEmpty())
+			event.getPlayer()
+					.sendMessage(
+							ChatColor.GOLD
+									+ "Something wait you in your chest (command /gp c to see it)");
+	}
+
+}
--- src/com/Balor/bukkit/GiftPost/GiftPost.java
+++ src/com/Balor/bukkit/GiftPost/GiftPost.java
@@ -21,8 +21,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
 import static com.Balor.utils.Display.sendHelp;
 //Permissions imports
 import com.nijiko.permissions.PermissionHandler;
@@ -37,6 +40,7 @@ public class GiftPost extends JavaPlugin
 
     public static final Logger log = Logger.getLogger("Minecraft");
     private GiftPostWorker gpw;
+    private GPPlayerListener pListener;
     /**
      * Permission plugin
      */
@@ -92,7 +96,10 @@ public class GiftPost extends JavaPlugin
     {
         setupPermissions();
         gpw = new GiftPostWorker(Permissions);
+        pListener=new GPPlayerListener(gpw);
         registerCommands();
+		PluginManager pm = getServer().getPluginManager();        
+        pm.registerEvent(Event.Type.PLAYER_JOIN, pListener, Priority.Normal, this);
     }
 
     @Override
